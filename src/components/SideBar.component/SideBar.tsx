9380253878214f0a5026e4fccf8603e797392325
Ramandeep <ramank6238299@gmail.com>
2022-09-08 10:45:59 +0530

    font end

--- src/components/SideBar.component/SideBar.tsx
+++ src/components/SideBar.component/SideBar.tsx
@@ -1,9 +1,9 @@
 import "./side-bar.css";
-import { BiHomeSmile, BiBookmark } from "react-icons/bi";
-import { MdOutlineTravelExplore } from "react-icons/md";
-import { RiNotificationLine } from "react-icons/ri";
+import { BiHomeSmile } from "react-icons/bi";
+import { MdLabelOutline } from "react-icons/md";
+import { FiTrash } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
-import { IoIosLogOut } from "react-icons/io";
+import { BsArchive } from "react-icons/bs";
 // import { useDispatch, useSelector } from "react-redux";
 // import { logout } from "../../redux/reducers/authSlice";
 import { Link, NavLink } from "react-router-dom";
@@ -12,81 +12,50 @@ export const SideBar = () => {
   //   const dispatch = useDispatch();
   return (
     <>
-      <div className="container-main">
-        <div className="left-aside-position">
-          <div className="left-aside-position">
-            <nav className="lf-aside">
-              <ul className="lf-aside-items">
-                <Link to="/home">
-                  <li className="lf-aside-item mgT-8">
-                    <BiHomeSmile />
-                    <span>Home</span>
-                  </li>
-                </Link>
+      <nav className="lf-aside">
+        <ul className="lf-aside-items">
+          <Link to="/home">
+            <li className="lf-aside-item mgT-8">
+              <BiHomeSmile />
+              <span>Home</span>
+            </li>
+          </Link>
 
-                <Link to="/explore">
-                  <li className="lf-aside-item mgT-8">
-                    <MdOutlineTravelExplore />
-                    <span>Explore</span>
-                  </li>
-                </Link>
+          <Link to="/explore">
+            <li className="lf-aside-item mgT-8">
+              <MdLabelOutline />
+              <span>Label</span>
+            </li>
+          </Link>
 
-                <Link to="/bookmarks">
-                  <li className="lf-aside-item mgT-8">
-                    <BiBookmark />
-                    <span>Bookmarks</span>
-                  </li>
-                </Link>
+          <Link to="/bookmarks">
+            <li className="lf-aside-item mgT-8">
+              <BsArchive />
+              <span>Archives</span>
+            </li>
+          </Link>
 
-                <Link to="/">
-                  <li className="lf-aside-item mgT-8">
-                    <CgProfile />
-                    <span>Profile</span>
-                  </li>
-                </Link>
+          <Link to="/">
+            <li className="lf-aside-item mgT-8">
+              <FiTrash />
+              <span>Trash</span>
+            </li>
+          </Link>
 
-                <Link to="/home">
-                  <li className="lf-aside-item mgT-8">
-                    <button className="btn bg-pur width100">
-                      Create New Post
-                    </button>
-                  </li>
-                </Link>
-              </ul>
-              <div className="profile">
-                <ul className="list border displayF space-between ppl-yk mgT-16">
-                  <Link to="/">
-                    <div className="list-start-sec displayF">
-                      <img
-                        // src={user.profilePhoto}
-                        alt="..."
-                        className="avatar avatar-s cursor"
-                      />
-                      <li className="list-items list-width">
-                        {/* {user.firstName} */}
-                        <span className="list-sec-text">
-                          @{/* {user.username} */}
-                        </span>
-                      </li>
-                    </div>
-                  </Link>
-                  <div className="list-end-sec">
-                    <p
-                      //   onClick={() => {
-                      //     console.log("logout ");
-                      //     dispatch(logout());
-                      //   }}
-                      className="follow capitalize cursor logout"
-                    >
-                      <IoIosLogOut />
-                    </p>
-                  </div>
-                </ul>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </div>
+          <Link to="/">
+            <li className="lf-aside-item mgT-8">
+              <CgProfile />
+              <span>Profile</span>
+            </li>
+          </Link>
+
+          <Link to="/home">
+            <li className="lf-aside-item mgT-8">
+              <button className="btn bg-pur width100">Create New note</button>
+            </li>
+          </Link>
+        </ul>
+      </nav>
     </>
   );
 };
